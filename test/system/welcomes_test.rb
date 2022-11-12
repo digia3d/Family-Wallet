0b578c7f9dcff2f1b73c72269ed77e93ea728e3d
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-12 18:24:58 +0100

    Generate with scaffold files for splash-welcome screen

---
+++ test/system/welcomes_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class WelcomesTest < ApplicationSystemTestCase
+  setup do
+    @welcome = welcomes(:one)
+  end
+
+  test "visiting the index" do
+    visit welcomes_url
+    assert_selector "h1", text: "Welcomes"
+  end
+
+  test "should create welcome" do
+    visit welcomes_url
+    click_on "New welcome"
+
+    fill_in "Index", with: @welcome.index
+    click_on "Create Welcome"
+
+    assert_text "Welcome was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Welcome" do
+    visit welcome_url(@welcome)
+    click_on "Edit this welcome", match: :first
+
+    fill_in "Index", with: @welcome.index
+    click_on "Update Welcome"
+
+    assert_text "Welcome was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Welcome" do
+    visit welcome_url(@welcome)
+    click_on "Destroy this welcome", match: :first
+
+    assert_text "Welcome was successfully destroyed"
+  end
+end
